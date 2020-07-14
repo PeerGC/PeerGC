0e92d142601c39a27d46dae60b3fa638ab2ccaa5
Artemas Radik <ajradik@gmail.com>
2020-07-13 20:22:22 -0400

    updated race and gender VC's

--- PeerGC/View Controllers/Additional Account Information Flow/GenderViewController.swift
+++ PeerGC/View Controllers/Additional Account Information Flow/GenderViewController.swift
@@ -13,101 +13,26 @@ class GenderViewController: UIViewController {
     
     @IBOutlet var specificationButtons: [DesignableButton]!
     
-    @IBOutlet weak var continueButton: DesignableButton!
-    
     @IBOutlet weak var mainLabel: UILabel!
     
     @IBOutlet weak var subLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        if AccountTypeViewController.data.accountType == "Student" {
-            subLabel.text = "EDyou uses this information to match you with tutors who are the same gender as you."
-        }
-        
-        else if AccountTypeViewController.data.accountType == "Tutor" {
-            subLabel.text = "EDyou uses this information to match you with students who are the same gender as you."
-        }
-        
-        AccountTypeViewController.data.gender = "Female"
-        
-        //
         for button in specificationButtons {
             button.titleLabel!.font = button.titleLabel!.font.withSize( (1.7/71) * UIScreen.main.bounds.height)
         }
-        
-        continueButton.titleLabel!.font = continueButton.titleLabel!.font.withSize( (1.7/71) * UIScreen.main.bounds.height)
         
         mainLabel.font = mainLabel.font.withSize( (3.5/71) * UIScreen.main.bounds.height)
         
         subLabel.font = subLabel.font.withSize( (1.7/71) * UIScreen.main.bounds.height)
         
-        for i in 0 ..< specificationButtons.count {
-           
-            if i == 0 {
-                specificationButtons[i].alpha = 1.0
-                specificationButtons[i].backgroundColor = UIColor.systemGreen
-            }
-            
-            else {
-                specificationButtons[i].alpha = 0.6
-                specificationButtons[i].backgroundColor = UIColor.systemPink
-            }
-            
-        }
-        
     }
     
     @IBAction func buttonPressed(_ sender: UIButton) {
         
-        if sender == continueButton {
-            UIView.animate(withDuration: 0.2) {
-                sender.alpha = 1.0
-            }
-            return
-        }
-        
         AccountTypeViewController.data.gender = sender.titleLabel!.text
         
-        
-        for button in specificationButtons {
-            
-            if button == sender {
-                button.backgroundColor = UIColor.systemGreen
-                UIView.animate(withDuration: 0.2) {
-                    sender.alpha = 1.0
-                }
-            }
-            
-            else {
-                button.backgroundColor = UIColor.systemPink
-                UIView.animate(withDuration: 0.2) {
-                    button.alpha = 0.6
-                }
-            }
-            
-        }
-        
-    }
-    
-    var mostRecentAlpha = 1.0
-    
-    @IBAction func buttonTouchDown(_ sender: UIButton) {
-        
-        self.mostRecentAlpha = Double(sender.alpha)
-        
-        UIView.animate(withDuration: 0.2) {
-            sender.alpha = 0.6
-        }
-        
-    }
-    
-    @IBAction func buttonCancel(_ sender: UIButton) {
-        
-        UIView.animate(withDuration: 0.2) {
-            sender.alpha = CGFloat(self.mostRecentAlpha)
-        }
     }
     
 }
--- PeerGC/View Controllers/Additional Account Information Flow/RaceViewController.swift
+++ PeerGC/View Controllers/Additional Account Information Flow/RaceViewController.swift
@@ -14,22 +14,12 @@ class RaceViewController: UIViewController {
     
     @IBOutlet var specificationButtons: [DesignableButton]!
     
-    @IBOutlet weak var continueButton: DesignableButton!
-    
     @IBOutlet weak var mainLabel: UILabel!
     
     @IBOutlet weak var subLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        if AccountTypeViewController.data.accountType == "Student" {
-            subLabel.text = "EDyou uses this information to match you with tutors who are the same race as you."
-        }
-        
-        else if AccountTypeViewController.data.accountType == "Tutor" {
-            subLabel.text = "EDyou uses this information to match you with students who are the same race as you."
-        }
         
         AccountTypeViewController.data.race = "Asian American"
         
@@ -38,105 +28,46 @@ class RaceViewController: UIViewController {
             button.titleLabel!.font = button.titleLabel!.font.withSize( (1.7/71) * UIScreen.main.bounds.height)
         }
         
-        continueButton.titleLabel!.font = continueButton.titleLabel!.font.withSize( (1.7/71) * UIScreen.main.bounds.height)
-        
         mainLabel.font = mainLabel.font.withSize( (3.5/71) * UIScreen.main.bounds.height)
         
         subLabel.font = subLabel.font.withSize( (1.7/71) * UIScreen.main.bounds.height)
-        
-        for i in 0 ..< specificationButtons.count {
-           
-            if i == 0 {
-                specificationButtons[i].alpha = 1.0
-                specificationButtons[i].backgroundColor = UIColor.systemGreen
-            }
-            
-            else {
-                specificationButtons[i].alpha = 0.6
-                specificationButtons[i].backgroundColor = UIColor.systemPink
-            }
-            
-        }
         
     }
     
     @IBAction func buttonPressed(_ sender: UIButton) {
         
-        if sender == continueButton {
-            UIView.animate(withDuration: 0.2) {
-                sender.alpha = 1.0
-            }
+    
             
-            let uid = Auth.auth().currentUser!.uid
-            let url = Auth.auth().currentUser!.photoURL
-            
-            var urlString = ""
-            
-            if url != nil {
-                urlString = "\(Auth.auth().currentUser!.photoURL!)"
-            }
-            
-            Firestore.firestore().collection("users").document(uid).setData(
-                ["accountType": AccountTypeViewController.data.accountType!,
-                 "zipCode": AccountTypeViewController.data.zipCode!,
-                 "value": AccountTypeViewController.data.value!,
-                 "gender": AccountTypeViewController.data.gender!,
-                 "interest": AccountTypeViewController.data.interest!,
-                 "race": AccountTypeViewController.data.race!,
-                 "firstName": Auth.auth().currentUser!.displayName!.split(separator: " ")[0],
-                 "photoURL": urlString ]
-                ) { (error) in
-            
-                self.performSegue(withIdentifier: "goToMatching", sender: self)
-                
-                if error != nil {
-                    // Show error message
-                    print("Error saving user data")
-                }
-            }
-            
-        }
+//        let uid = Auth.auth().currentUser!.uid
+//        let url = Auth.auth().currentUser!.photoURL
+//
+//        var urlString = ""
+//
+//        if url != nil {
+//            urlString = "\(Auth.auth().currentUser!.photoURL!)"
+//        }
+//
+//        Firestore.firestore().collection("users").document(uid).setData(
+//            ["accountType": AccountTypeViewController.data.accountType!,
+//             "zipCode": AccountTypeViewController.data.zipCode!,
+//             "value": AccountTypeViewController.data.value!,
+//             "gender": AccountTypeViewController.data.gender!,
+//             "interest": AccountTypeViewController.data.interest!,
+//             "race": AccountTypeViewController.data.race!,
+//             "firstName": Auth.auth().currentUser!.displayName!.split(separator: " ")[0],
+//             "photoURL": urlString ]
+//            ) { (error) in
+//
+//            self.performSegue(withIdentifier: "goToMatching", sender: self)
+//
+//            if error != nil {
+//                // Show error message
+//                print("Error saving user data")
+//            }
+//        }
         
         AccountTypeViewController.data.race = sender.titleLabel!.text
         
-        
-        for button in specificationButtons {
-            
-            if button == sender {
-                button.backgroundColor = UIColor.systemGreen
-                UIView.animate(withDuration: 0.2) {
-                    sender.alpha = 1.0
-                }
-            }
-            
-            else {
-                button.backgroundColor = UIColor.systemPink
-                UIView.animate(withDuration: 0.2) {
-                    button.alpha = 0.6
-                }
-            }
-            
-        }
-        
-    }
-    
-    var mostRecentAlpha = 1.0
-    
-    @IBAction func buttonTouchDown(_ sender: UIButton) {
-        
-        self.mostRecentAlpha = Double(sender.alpha)
-        
-        UIView.animate(withDuration: 0.2) {
-            sender.alpha = 0.6
-        }
-        
-    }
-    
-    @IBAction func buttonCancel(_ sender: UIButton) {
-        
-        UIView.animate(withDuration: 0.2) {
-            sender.alpha = CGFloat(self.mostRecentAlpha)
-        }
     }
     
 }
