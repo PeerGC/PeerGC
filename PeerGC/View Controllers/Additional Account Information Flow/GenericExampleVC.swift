5454c9b4b2570296dc655296d9a01dffdf087b77
Artemas Radik <ajradik@gmail.com>
2020-07-22 20:05:40 -0400

    view showing

--- PeerGC/View Controllers/Additional Account Information Flow/GenericExampleVC.swift
+++ PeerGC/View Controllers/Additional Account Information Flow/GenericExampleVC.swift
@@ -11,8 +11,39 @@ import UIKit
 
 class GenericExampleVC: GenericStructureViewController {
     override func viewDidLoad() {
-        
+        genericStructureViewControllerMetadataDelegate = self
+        buttonsDelegate = self
+        super.viewDidLoad()
     }
 }
 
+extension GenericExampleVC: GenericStructureViewControllerMetadataDelegate {
+    
+    func databaseIdentifier() -> String {
+        "ExampleID"
+    }
+    
+    func title() -> String {
+        "Hello! This is what a title might look like."
+    }
+    
+    func subtitle() -> String? {
+        "This is where we can explain stuff or whatever. We don't use your data."
+    }
+    
+    func nextViewController() -> UIViewController {
+        return UIViewController()
+    }
+    
+}
 
+extension GenericExampleVC: ButtonsDelegate {
+    func buttons() -> [String] {
+        return [
+            "Yes, I have done this.",
+            "No, I have not done this.",
+            "I may have done this?",
+            "Absolutely not."
+        ]
+    }
+}
