1a3bf6985d95f4eddce1038bcb6e564252802382
Artemas Radik <ajradik@gmail.com>
2020-07-25 20:04:12 -0400

    added firstName to db

--- PeerGC/View Controllers/Questions Flow/MatchingVC.swift
+++ PeerGC/View Controllers/Questions Flow/MatchingVC.swift
@@ -39,6 +39,7 @@ class MatchingVC: GenericStructureViewController {
         }
         
         GenericStructureViewController.sendToDatabaseData["photoURL"] = photoURLString
+        GenericStructureViewController.sendToDatabaseData["firstName"] = Auth.auth().currentUser!.displayName!
         
         //Upload Data
         
